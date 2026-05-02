9efe2089654f082d9022bf00dbf9733d16f12f30
skurtyyskirts <148610406+skurtyyskirts@users.noreply.github.com>
2026-05-02 14:47:34 -0500

    Add unit tests for ResponseUtility functions
    Add missing unit tests covering empty list, single path, multiple paths, and path-without-directory cases for both formatting functions. Also create tests __init__.py.

--- ChatRTX_App/ChatRTXUI/engine/tests/test_ResponseUtility.py
+++ ChatRTX_App/ChatRTXUI/engine/tests/test_ResponseUtility.py
@@ -9,7 +9,7 @@ import sys
 import os.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from ResponseUtility import getLocalLinksMarkdown
+from ResponseUtility import getLocalLinksMarkdown, getImagesMarkdown
 
 def test_getLocalLinksMarkdown_empty_list():
     assert getLocalLinksMarkdown([]) == '<div class="links-list"></div>'
@@ -31,3 +31,23 @@ def test_getLocalLinksMarkdown_windows_paths():
         paths = ['C:\\Users\\test\\Documents\\file.pdf', 'D:\\data\\info.csv']
         expected = '<div class="links-list"><a data-link=\'C:\\Users\\test\\Documents\\file.pdf\'>file.pdf</a><a data-link=\'D:\\data\\info.csv\'>info.csv</a></div>'
         assert getLocalLinksMarkdown(paths) == expected
+
+def test_getLocalLinksMarkdown_no_directory():
+    paths = ['test.txt']
+    expected = '<div class="links-list"><a data-link=\'test.txt\'>test.txt</a></div>'
+    assert getLocalLinksMarkdown(paths) == expected
+
+def test_getImagesMarkdown_empty_list():
+    paths = []
+    expected = '<div class="images-list"></div>'
+    assert getImagesMarkdown(paths) == expected
+
+def test_getImagesMarkdown_single_path():
+    paths = ['/foo/bar/image.png']
+    expected = '<div class="images-list"><a data-link=\'/foo/bar/image.png\'><img src=\'/foo/bar/image.png\'></a></div>'
+    assert getImagesMarkdown(paths) == expected
+
+def test_getImagesMarkdown_multiple_paths():
+    paths = ['/foo/bar/image1.jpg', '/foo/baz/image2.png']
+    expected = '<div class="images-list"><a data-link=\'/foo/bar/image1.jpg\'><img src=\'/foo/bar/image1.jpg\'></a><a data-link=\'/foo/baz/image2.png\'><img src=\'/foo/baz/image2.png\'></a></div>'
+    assert getImagesMarkdown(paths) == expected
